dc91af84547f72304ccb604a1c4622393fdef922
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-04-29 12:50:56 +0300

    Changes to activate the bug

--- reformat-me-with-only-vcs-ticked.sql
+++ reformat-me-with-only-vcs-ticked.sql
@@ -2,4 +2,4 @@
 
    select 42;
 
-select 'edit me and notice lines 1 and 2 to change with reformat only vcs changed text'
+   select 'edit me and notice lines 1 and 2 to change with reformat only vcs changed text' WHERE 1=1;
